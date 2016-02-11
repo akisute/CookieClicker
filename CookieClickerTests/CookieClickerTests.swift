ef68eed45565511887da3863f8913261954c0f73
akisute <akisutesama@gmail.com>
2016-02-11 14:51:38 +0900

    Initial import

---
+++ CookieClickerTests/CookieClickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CookieClickerTests.swift
+//  CookieClickerTests
+//
+//  Created by 小野 将司 on 2016/02/11.
+//  Copyright © 2016年 akisute. All rights reserved.
+//
+
+import XCTest
+@testable import CookieClicker
+
+class CookieClickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
