3ac07c4ba9829512d27117b2625f2eb6f2e9c421
akisute <akisutesama@gmail.com>
2016-02-11 18:40:49 +0900

    Implementing Basic UI

---
+++ CookieClicker/ViewControllers/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  CookieClicker
+//
+//  Created by 小野 将司 on 2016/02/11.
+//  Copyright © 2016年 akisute. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+}
---
+++ CookieClicker/ViewControllers/UpgradesViewController.swift
@@ -0,0 +1,47 @@
+//
+//  UpgradesViewController.swift
+//  CookieClicker
+//
+//  Created by 小野 将司 on 2016/02/11.
+//  Copyright © 2016年 akisute. All rights reserved.
+//
+
+import UIKit
+
+class UpgradesViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+}
+
+extension UpgradesViewController {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension UpgradesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+}
